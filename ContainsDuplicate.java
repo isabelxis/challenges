3fa7c61f3df8be37e7ea434b84e988b2401865fd
Isabel Xavier <isabelxis@gmail.com>
2025-11-23 16:58:19 -0300

    Add external resource links as comments
    Added links to external resources in ContainsDuplicate.java

---
+++ ContainsDuplicate.java
@@ -0,0 +1,42 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/*/
+https://neetcode.io/quiz/contains-duplicate
+https://www.jdoodle.com/online-java-compiler
+/*/
+
+public class ContainsDuplicate {
+    public boolean nextDuplicate(int[] nums){
+
+        if (nums == null || nums.length <= 1) {
+            return false;
+        }
+
+        Set<Integer> hash = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int currentNum = nums[i];
+
+            if (hash.contains(currentNum)) {
+                return true;
+            }
+            hash.add(currentNum);
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        ContainsDuplicate check = new ContainsDuplicate();
+
+        int[] nums1 = {1,2,3,1};
+        System.out.println(check.nextDuplicate(nums1)); // Output: true
+
+        int[] nums2 = {1,2,3,4};
+        System.out.println(check.nextDuplicate(nums2)); // Output: true
+
+    }
+
+}

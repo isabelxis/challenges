9aa3c454b10ea79d515663016d14635336d12d9c
Isabel Xavier <isabelxis@gmail.com>
2025-12-06 12:24:17 -0300

    Implement character counting for grouping anagrams
    Refactor groupAnagrams method to use character counting for anagrams.

--- AgrupaAnagam.java
+++ AgrupaAnagam.java
@@ -11,19 +11,28 @@ public class AgrupaAnagam {
 
        Map<String,List<String> > map = new HashMap<>();
 
+        //para cada elemento do array de strs atribua a 's' e execute os comandos
        for(String s : strs){
+        // cria um array de 26 posições referente ao alfabeto americano
         int[] count = new int[26];
+        // atribua cada letra desse elemento para a variavel 'c' e execute o comando
         for(char c : s.toCharArray()){
+           //conta quantas vezes a letra atual aparece no array do count 
            count[c-'a']++;            
         }
 
+        //reserva na memória a quantidade de referente a 26 letras do alfabeto e a virgula que separa a quantidade de letras do count
         StringBuilder sb = new StringBuilder(52);
+           
+        // atribua cada elemnto do count para a variavel 'x' e execute o comando
         for (int x : count){
             sb.append(x).append(',');
         }
 
+        //Ttransforma o resultado do sb em String imutavel para o tablehash
         String key = sb.toString();
 
+        //verifica se a key já existe no hash, se existir adciona no array correspondente, se não cria um novo array
         map.computeIfAbsent(key,k-> new ArrayList<>()).add(s);
 
        }
